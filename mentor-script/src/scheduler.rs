8336684620cde2e71d5df881374f52b1f0bc8281
Cayden Tisdale <caydentisdale43@gmail.com>
2026-01-30 20:31:41 -0500

    refactor scheduler to be more robust in handling time with useful info

--- mentor-script/src/scheduler.rs
+++ mentor-script/src/scheduler.rs
@@ -1,4 +1,5 @@
-use chrono::Timelike;
+use std::arch::aarch64::int64x1x3_t;
+use chrono::{DateTime, Local, Timelike};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CheckType {
@@ -14,5 +15,17 @@ pub fn check_time() -> Option<CheckType> {
         30 => Some(CheckType::HalfHour),
         55 => Some(CheckType::Hour),
         _ => None,
+    }
+
+}
+
+pub fn minutes_until_next_check(now: DateTime<Local>) -> (CheckType, i64) {
+    let m = now.minute();
+    if m < 30 {
+        (CheckType::HalfHour, (30 - m) as i64)
+    } else if m < 55 {
+        (CheckType::Hour, (55 - m) as i64)
+    } else {
+        (CheckType::HalfHour, (60 - m + 30) as i64)
     }
 }
